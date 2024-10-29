c39e90526f353a8f01e3ea60ac33b447f930891e
iogurte-21 <igbob21@gmail.com>
2024-10-28 21:11:59 -0300

    Update processos.h

--- processos.h
+++ processos.h
@@ -5,10 +5,12 @@
 using namespace std;
 
 /**
- * Definição da struct processos
- * 
- * 
- * 
+ * @brief Definição da struct processos
+ * @param id Identificador criado para o processo.
+ * @param duracao Tempo de duração do proceso.
+ * @param start_time Tempo de início do processo.
+ * @param end_time Tempo de finalização do processo.
+ * @param estado Estado do processo no processador.
  */
 struct processos{
     int id;
@@ -21,17 +23,20 @@ struct processos{
 };
 
 /**
- * @brief 
- * @details 
- * @return 
+ * @brief Função responsável pela criação dos processos.
+ * @details Cria os processos.
+ * @param duracao Tempo de duração do processo.
+ * @param inicio Tempo de início do processo.
+ * @return Retorna um novo processo.
  */
 processos cria_process(int duracao, int inicio);
 
 /**
- * @brief 
- * @details 
- * @return 
+ * @brief Inicializa o programa.
+ * @details Define tempos de duração e início aleatórios para os processos e os organiza.
+ * @param quantidade_processos Quantidade de processos do programa.
+ * @return Retorna o programa a ser escalonado.
  */
-vector<processos> inicia_programa(int quantidade_procesoss);
+vector<processos> inicia_programa(int quantidade_processos);
 
 #endif 
