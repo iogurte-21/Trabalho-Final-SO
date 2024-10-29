e54450cc2bc564bf5cb096be747425e18201bc9b
Artur Frederico Heiber <136103364+Sbugart@users.noreply.github.com>
2024-10-28 21:12:25 -0300

    Update processos.cpp

--- processos.cpp
+++ processos.cpp
@@ -5,12 +5,12 @@ processos cria_process(int duracao, int inicio){
     return novo;
 }
 
-vector<processos> inicia_programa(int quantidade_procesoss){
+vector<processos> inicia_programa(int quantidade_processos){
     vector<processos> programa;
     int duracao, inicio;
     srand(time(0));
 
-    for(int i = 0; i < quantidade_procesoss; i++){
+    for(int i = 0; i < quantidade_processos; i++){
         duracao = 1 + rand() % 25;
         inicio = rand() % 20;
         programa.push_back(cria_process(duracao, inicio));
@@ -20,7 +20,7 @@ vector<processos> inicia_programa(int quantidade_procesoss){
         return a.start_time < b.start_time;
     });
 
-    for(int i = 0; i < quantidade_procesoss; i++) programa[i].id = i + 1;
+    for(int i = 0; i < quantidade_processos; i++) programa[i].id = i + 1;
     
     return programa;
 }
